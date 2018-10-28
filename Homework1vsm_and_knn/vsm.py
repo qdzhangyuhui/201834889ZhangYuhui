2bcc512cf61019d9f01935bbadca304a3374ac63
qdzhangyuhui <41944541+qdzhangyuhui@users.noreply.github.com>
2018-10-28 17:10:14 +0800

    test

--- Homework1vsm_and_knn/vsm.py
+++ Homework1vsm_and_knn/vsm.py
@@ -3,32 +3,45 @@ import os
 import nltk
 import re
 import string
+
 rootpath = '..\\datatest'
-wordlist=[]
-#遍历文件夹
-for folderlists in os.listdir(rootpath):
-    #print(folderlists)
-    path = os.path.join(rootpath, folderlists)
-    #print(path)
-    for file in os.listdir(path):
-        #print(file)
-        filepath = os.path.join(path,file)
-        if os.path.isfile(filepath):
-           with open(filepath, mode='r',encoding='latin-1') as f:
-               document = f.read()
-           f.close()
-           #大写转小写
-           document=document.lower()
-           # tokenization and normalization
-           tokens = nltk.word_tokenize(document)
-           print(tokens)
-           #normalization
-           #print(document)
-           #for token in tokens:
-               #if token and
+wordlist = []
+
+
+# 遍历文件夹
+
 
 def main():
-   print()
+    for folderlists in os.listdir(rootpath):
+        # print(folderlists)
+        path = os.path.join(rootpath, folderlists)
+        # print(path)
+        for file in os.listdir(path):
+            # print(file)
+            filepath = os.path.join(path, file)
+            if os.path.isfile(filepath):
+                with open(filepath, mode='r', encoding='latin-1') as f:
+                    document = f.read()
+                f.close()
+                # tokenizaton and normalization
+                document = document.lower()  # 大写转小写
+                tokens = nltk.word_tokenize(document)  # tokenizaton
+                punctuation_remove = re.compile('[%s]' % re.escape(string.punctuation))  # 去标点符号
+                tokens = list(filter(lambda word: word != "", [punctuation_remove.sub("", word) for word in tokens]))
+                print(tokens)
+                stem = nltk.stem.LancasterStemmer()  # 提取词干
+                # print(tokens)
+                for token in tokens:
+                    token = stem.stem(token)#Stemming
+                    #print(token)
+                    #wordlist.append(token)
+                    if token not in wordlist:
+                       wordlist.append(token)
+    with open('word.txt', 'w') as f:
+        for w in wordlist:
+            f.write(w + '\n')
+    f.close()
+
 
 if __name__ == '__main__':
     main()
