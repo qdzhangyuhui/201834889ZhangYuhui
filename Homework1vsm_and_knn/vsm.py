c030d2996ca25b4ac4cbee3cec6bb978556092ab
qdzhangyuhui <41944541+qdzhangyuhui@users.noreply.github.com>
2018-10-28 15:30:37 +0800

    Update Homework1vsm_and_knn/vsm.py

--- Homework1vsm_and_knn/vsm.py
+++ Homework1vsm_and_knn/vsm.py
@@ -4,22 +4,28 @@ import nltk
 import re
 import string
 rootpath = '..\\datatest'
+wordlist=[]
 #遍历文件夹
 for folderlists in os.listdir(rootpath):
-    print(folderlists)
+    #print(folderlists)
     path = os.path.join(rootpath, folderlists)
-    print(path)
+    #print(path)
     for file in os.listdir(path):
-        print(file)
+        #print(file)
         filepath = os.path.join(path,file)
         if os.path.isfile(filepath):
            with open(filepath, mode='r',encoding='latin-1') as f:
                document = f.read()
            f.close()
-           #tokenization
+           #大写转小写
+           document=document.lower()
+           # tokenization and normalization
            tokens = nltk.word_tokenize(document)
            print(tokens)
+           #normalization
            #print(document)
+           #for token in tokens:
+               #if token and
 
 def main():
    print()
