d860e7923760a7f9ff87cf5d5b8e58e13a3be56a
qdzhangyuhui <41944541+qdzhangyuhui@users.noreply.github.com>
2018-11-05 17:23:18 +0800

    the Second method of knn
    use sklearn 、neighbors

--- Homework1vsm_and_knn/knnn.py
+++ Homework1vsm_and_knn/knnn.py
@@ -4,39 +4,26 @@ from collections import Counter
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import accuracy_score
 from sklearn import neighbors
-def load_train_data(path):
+def load_data(path):
     train=np.loadtxt(path,delimiter=",", skiprows=0)
-    #print(train.shape)
-    divide=int((train.shape[0])*0.6)
-    #print(divide)
-    vec_train=train[0:divide,0:-1]
-    vec_test = train[divide:train.shape[0], 0:-1]
-    label=train[0:divide,-1]
+    vec_train=train[0:,0:5]
+    label=train[0:,-1]
     labels=[int(i) for i in label]
-    print(type(labels)	)
-    print(vec_test)
-    return vec_test,vec_train,labels
+    #print(type(labels)	)
+    return vec_train,labels
 def main():
     print('knn')
-    vec_test,vec ,labels = load_train_data("vector7.csv")
-    print('################')
-    knn = neighbors.KNeighborsClassifier(n_neighbors=1, metric='euclidean')  # 取得knn分类器
-    print(vec)
+    vec_train,labels1 = load_data("vector7.csv")
+    vec_test,labels2 = load_data("vector8.csv")
+    #vec_test,labels2=
+    knn = neighbors.KNeighborsClassifier(n_neighbors=2, metric='euclidean')  # 取得knn分类器
+    print(vec_train)
     print(vec_test)
-    labels = np.array(labels)  #
-    print(labels)
-    data_test=np.array(vec_test)
-    print(data_test)
-    knn.fit(vec, labels)  # 导入数据进行训练
-    print(knn.predict(data_test))
-    #data = np.array([[3, 104], [2, 100], [1, 81], [101, 10], [99, 5], [98, 2]])
-    #print(data)
-    #labels = np.array([1, 1, 1, 2, 2, 2])  # labels则是对应Romance和Action
-    #print(labels)
-    #knn.fit(data, labels)  # 导入数据进行训练
-    #print(knn.predict([[18, 90]]))
-    #print([[18, 90]])
-
-
+    labels1 = np.array(labels1)  #
+    labels2 = np.array(labels2)
+    print(labels1)
+    print(labels2)
+    knn.fit(vec_train, labels1)  # 导入数据进行训练
+    #print(knn.predict(vec_train))
 if __name__ == '__main__':
     main()
