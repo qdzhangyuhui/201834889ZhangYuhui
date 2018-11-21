5b6261aa25c655d8fdec562d63c93e8b4a43647a
qdzhangyuhui <41944541+qdzhangyuhui@users.noreply.github.com>
2018-11-21 15:54:00 +0800

    NBC

--- Homework2NBC/NBC.py
+++ Homework2NBC/NBC.py
@@ -1,6 +1,12 @@
 import os
+path = '..\data'
+docs = []#存储处理好的文档
+words = []#词汇表
+dict = [] #词典表(过滤后）
 def main():
     print('NBC')
+def dataprocessing(path):
+    print('input data')
 
 if __name__ == '__main__':
     main()
