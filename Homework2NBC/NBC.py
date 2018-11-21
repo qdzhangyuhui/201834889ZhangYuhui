4551405efec737ac19f5833c2cf42dd65eb61924
qdzhangyuhui <41944541+qdzhangyuhui@users.noreply.github.com>
2018-11-21 16:30:01 +0800

    NBC

--- Homework2NBC/NBC.py
+++ Homework2NBC/NBC.py
@@ -1,13 +1,21 @@
 import os
+import nltk
+import re
+import string
+import random
+from collections import Counter
+import numpy as np
 rootpath = '..\data'
 docs = []#存储处理好的文档
 words = []#词汇表
 dict = [] #词典表(过滤后）
-
+labels = []
+countlist = {}
 def dataprocessing(rootpath):
     print('input data')
     for folderlists in os.listdir(rootpath):
         # print(folderlists)
+        label = 1
         path = os.path.join(rootpath, folderlists)
         # print(path)
         for file in os.listdir(path):
@@ -20,6 +28,29 @@ def dataprocessing(rootpath):
                 f.close()
                 # 文件内容处理
                 document = document.lower()  # 大写转小写
+                tokens = nltk.word_tokenize(document)  # tokenizaton
+                punctuation_remove = re.compile('[%s]' % re.escape(string.punctuation))  # 去标点符号
+                tokens = list(filter(lambda word: word != "", [punctuation_remove.sub("", word) for word in tokens]))
+                # print(tokens)
+                stopwords = set(nltk.corpus.stopwords.words("english"))  # 停用词
+                stem = nltk.stem.LancasterStemmer()  # 提取词干
+                # print(tokens)
+                for token in tokens:
+                    if not token.isdigit() and token.isalpha():  # 去除数字
+                        token = stem.stem(token)  # Stemming
+                        if token not in stopwords:  # 去停用词
+                            doc.append(token)  # 存储处理好的文章
+                            if token not in words:  # 去重
+                                words.append(token)
+                            if token not in countlist:
+                                countlist[token] = 1
+                            else:
+                                countlist[token] += 1
+                labels.append(label)  # 为文章打标签
+                docs.append(doc)
+        label += 1  # 一个文件夹为一个标签
+    print(docs)
+    print(words)
 def main():
     print('NBC')
 if __name__ == '__main__':
